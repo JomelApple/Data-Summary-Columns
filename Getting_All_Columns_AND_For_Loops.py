5394a58805a08cdc37a76f353d45bf18c6a7f67b
Jomel <79125032+JomelApple@users.noreply.github.com>
2021-02-15 19:12:07 -0800

    Add files via upload

---
+++ Getting_All_Columns_AND_For_Loops.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 15:59:36 2021
+
+@author: Meeko's Acer Laptop
+"""
+
+import pandas 
+
+df = pandas.read_csv("COVID-19_Case_Surveillance_Public_Use_Data.csv")
+
+f = open("data_summary_columns.txt", "w")
+
+all_col = list(df)
+
+length_col = len(all_col)
+
+for x in range(length_col): # ALWAYS END FOR LOOPS WITH COLON
+	print( all_col[x] )
+
+###
+#column_name = "race_ethnicity_combined"
+f.write(all_col + '\n')     # header line for the name of the column 
+
+for name,_ in df.groupby(all_col):  # use groupby to get the possible column values.
+    print(name)
+    f.write('\t' + name + "\n") # write the column value *indented* using \t, on its own line.
+    
+###
+
+f.close()
+
+
+####################
+
+# For Loop Practice: Basic level
+
+all_columns = ["qwerty", "asdf", "hello", "goodbye"]
+
+for x in range(4):
+	print( all_columns[x] )
+###
